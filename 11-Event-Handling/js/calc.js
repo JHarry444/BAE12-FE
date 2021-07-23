ed10e9eda6f9872c76da2092dc3bb48370073e88
jordan <jharrison1@qa.com>
2021-07-23 10:14:37 +0100

    added history to calculator

--- 11-Event-Handling/js/calc.js
+++ 11-Event-Handling/js/calc.js
@@ -7,6 +7,8 @@ const result = document.querySelector("#result");
 
 const buttons = document.querySelectorAll("div#calculator > button");
 
+const output = document.querySelector("#history");
+
 const calculate = (e) => {
     const operator = e.target.innerText;
 
@@ -15,7 +17,7 @@ const calculate = (e) => {
             number1.value = "0";
             number2.value = "0";
             result.value = "0";
-            break;
+            return;
         case '+':
             result.value = Number.parseFloat(number1.value) + Number.parseFloat(number2.value);
             break;
@@ -29,7 +31,15 @@ const calculate = (e) => {
             result.value = Number.parseFloat(number1.value) / Number.parseFloat(number2.value);
             break;
     }
+    writeHistory(number1.value, operator, number2.value, result.value);
 }
 
 // listens for a 'click' event on each button -> calling the calculate function when triggered
 buttons.forEach(button => button.addEventListener('click', calculate));
+
+const writeHistory = (num1, op, num2, result) => {
+    const newHistory = document.createElement('p');
+    newHistory.textContent = `${num1} ${op} ${num2} = ${result}`;
+
+    output.appendChild(newHistory);
+}

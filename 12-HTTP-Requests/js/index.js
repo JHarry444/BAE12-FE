83c69d5d1ce23a7cb7f478af96af8fb7ab7646c2
jordan <jharrison1@qa.com>
2021-07-23 15:33:53 +0100

    added getById example

--- 12-HTTP-Requests/js/index.js
+++ 12-HTTP-Requests/js/index.js
@@ -11,20 +11,46 @@ axios.get(`${baseURL}/`)
 
 console.log("Have we got a response yet?");
 
-const getAllSection = document.querySelector("#getAllSection");
+const getAllOutput = document.querySelector("#getAllOutput");
+const getByIdOutput = document.querySelector("#getByIdOutput");
+
+const kittenId = document.querySelector("#kittenId");
 
 axios.get(`${baseURL}/getAllKittens`)
     .then(res => {
         const kittens = res.data;
 
-        kittens.forEach(kitten => renderKitten(kitten));
+        getAllOutput.innerHTML = ""; // blanks an element
+
+        kittens.forEach(kitten => renderKitten(kitten, getAllOutput));
     }).catch(err => console.log(err));
 
 
-const renderKitten = (kitten) => {
-    const newKitten = document.createElement('p');
+const renderKitten = (kitten, outputDiv) => {
+    const newKitten = document.createElement('div');
 
-    newKitten.textContent = JSON.stringify(kitten);
+    const kittenName = document.createElement("h3");
+    kittenName.innerText = kitten.name;
+    newKitten.appendChild(kittenName);
 
-    getAllSection.appendChild(newKitten);
+    const kittenAge = document.createElement("p");
+    kittenAge.innerText = `Age: ${kitten.age}`;
+    newKitten.appendChild(kittenAge);
+
+    const kittenBreed = document.createElement("p");
+    kittenBreed.innerText = `Breed: ${kitten.breed}`; 
+    newKitten.appendChild(kittenBreed);
+
+    outputDiv.appendChild(newKitten);
 }
+
+const getKittenById = () => {
+    axios.get(`${baseURL}/getKitten/${kittenId.value}`)
+    .then(res => {
+        const kitten = res.data;
+        getByIdOutput.innerHTML = "";
+        renderKitten(kitten, getByIdOutput);
+    }).catch(err => console.log(err));
+}
+
+document.querySelector("section#getByIdSection > button").addEventListener('click', getKittenById);

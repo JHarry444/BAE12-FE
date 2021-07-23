62bbae884b148b8d63a79538af4b2dfd3a164724
jordan <jharrison1@qa.com>
2021-07-23 09:14:26 +0100

    added clacl example

---
+++ 11-Event-Handling/js/calc.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const number1 = document.querySelector("#number1");
+const number2 = document.querySelector("#number2");
+
+const result = document.querySelector("#result");
+
+const calculate = (e) => {
+    const operator = e.target.innerText;
+
+    switch (operator) {
+        case 'C':
+            number1.value = "0";
+            number2.value = "0";
+            result.value = "0";
+            break;
+        case '+':
+            result.value = Number.parseFloat(number1.value) + Number.parseFloat(number2.value);
+            break;
+        case '-':
+            result.value = Number.parseFloat(number1.value) - Number.parseFloat(number2.value);
+            break;
+        case '*':
+            result.value = Number.parseFloat(number1.value) * Number.parseFloat(number2.value);
+            break;
+        case '/':
+            result.value = Number.parseFloat(number1.value) / Number.parseFloat(number2.value);
+            break;
+    }
+}
+
